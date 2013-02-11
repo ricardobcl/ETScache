11f2dfefc36af38726d67bfa5eb811b59fcd42bc
ricardobcl <tome.wave@gmail.com>
2013-02-11 17:52:47 +0000

    New file name and update readme.

--- README.markdown
+++ README.markdown
@@ -5,56 +5,61 @@ ETScache is very(!) simple, self-contained memory cache, using only ETS tables i
 
 It has the following functions
 
-*	new (max_size) -> etscache
-*	put_new (etscache, key, value) -> ok ; {error, "Already exists!"}
+*	new (max_size) -> etscache()
+*	put_new (etscache, key, value) -> ok | already_exists
 *	update (etscache, key, value) -> ok | not_found
 *	get (etscache, key, timeout) -> {ok, value} | not_found | expired
 
 
 It is design to perform rapidly: _Get_ is constant **_O(1)_**; _Put\_New_ is constant **_O(1)_**; Update is linear with the number os elements in cache **_O(N)_**.
 
-Author
-------
+### Author
 
-Ricardo Gonçalves <tome.wave@gmail.com>
+Ricardo Tomé Gonçalves <tome.wave@gmail.com>
 
-How To Use
-----------
+### Contributions
 
-		% Create a new cache
-        C = etscache:new(128),      %start takes 1 argument, the maximum number of elememts in cache
+[Ali Yakamercan][Ali github]
 
-		% Add a key/value
-		ok = etscache:put_new(C, key1, "v1"), 
-		
-		% put_new only adds new values
-		{error,_} = etscache:put_new(C, key1, "cenas"), 
-		
-		% Update an existing value
-		etscache:update(C, key1, "v11"), 
-		
-		% Get the value for some key
-        Milliseconds = 10000
-        case etscache:get(C, key1, Milliseconds) of
-          not_found -> io:format("Value not Found!~n");
-          {ok, Value} -> io:format("Value found -> ~p~n", [Value]);
-          expired -> io:format("Value is Expired!~n");
-        end.
+### How To Use
+
+
+```Erlang
+% Create a new cache with the maximum number of elements in cache
+C = etscache:new(128),
+
+% Add a key/value
+ok = etscache:put_new(C, key1, "v1"), 
+
+% put_new only adds new values
+{error,_} = etscache:put_new(C, key1, "v2"), 
+
+% Update an existing value
+etscache:update(C, key1, "v11"), 
+
+% Get the value for some key
+Milliseconds = 10000,
+case etscache:get(C, key1, Milliseconds) of
+    not_found -> io:format("Value not Found!~n");
+    {ok, Value} -> io:format("Value found -> ~p~n", [Value]);
+    expired -> io:format("Value is Expired!~n")
+end.
+```
 
 
 Test It
 -------
 
-Function _test_ gives and example of a possible run, and output the cache in the end, to check its state.
+```bash
+$ erlc -DTEST etscache.erl 
+$ erl -eval "eunit:test(etscache)"
+```
 
-	~$ cd "ETScacheFolder"
-	~$ erlc -DTEST etscache.erl 
-	~$ erl -eval "eunit:test(etscache)"
-	
-	
 TODO
 ----
 
-*	Add proper testing framework. Look at <http://etrepum.github.com/erl_testing_2011/>
 *	Look at Judy C implemention http://judy.sourceforge.net/doc/index.html
 *	Benchmarks!!
+
+
+[Ali github]: https://github.com/aliyakamercan
--- ets_cache.erl
+++ ets_cache.erl
@@ -1,51 +1,49 @@
 
-%%-------------------------------------------------------------------
-%%
-%% File:      etscache.erl
-%%
-%% @author    Ricardo Gonçalves <tome.wave@gmail.com>
-%%
-%% @copyright 2011 Ricardo Tomé Gonçalves 
-%%
-%% This file is provided to you under the Apache License,
-%% Version 2.0 (the "License"); you may not use this file
-%% except in compliance with the License.  You may obtain
-%% a copy of the License at
-%%
-%%   http://www.apache.org/licenses/LICENSE-2.0
-%%
-%% Unless required by applicable law or agreed to in writing,
-%% software distributed under the License is distributed on an
-%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-%% KIND, either express or implied.  See the License for the
-%% specific language governing permissions and limitations
-%% under the License.
-%%
-%% @doc  
-%%	ETScache is very(!) simple in-memory cache, using ETS tables in Erlang. 
-%%  You can create a cache with a maximum number of elements in it, and when 
-%%  this limit is exceed, the oldest element is eliminated.
-%% @end  
-%%
-%%-------------------------------------------------------------------
+%%%-------------------------------------------------------------------
+%%%
+%%% File:      ets_cache.erl
+%%%
+%%% @author    Ricardo Tomé Gonçalves <tome.wave@gmail.com>
+%%%
+%%% @doc  
+%%% ets_cache is very(!) simple in-memory cache, using ETS tables in Erlang. 
+%%%  You can create a cache with a maximum number of elements in it, and when 
+%%%  this limit is exceed, the oldest element is eliminated.
+%%% @end  
+%%%
+%%%
+%%% The MIT License (MIT)
+%%% Copyright (C) 2013
+%%%
+%%% Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+%%%
+%%% The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+%%%
+%%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+%%%
+%%%-------------------------------------------------------------------
+
+-module(ets_cache).
+
+-export([   new/1,
+            put_new/3,
+            update/3,
+            get/3,
+            destroy/1
+        ]).
+
+-export_type([ets_cache/0]).
+
+-opaque ets_cache() :: #cache{}.
+-type key() :: any().
+-type value() :: any().
 
 
--module(etscache).
-
--author('Ricardo Tome Goncalves <tome@di.uminho.pt>').
-
--export([new/1, put_new/3, update/3, get/3, destroy/1]).
-
--type key() :: binary().
--type value() :: binary().
-
-
-%% Container for the cache
-%% It has 2 ets tables, and a fixed maximum size for the cache
+%% @doc Record for ets_cache. It has 2 ets tables, and a maximum size for the cache
 -record(cache, {
-	maxsize :: integer(),
-	table, % :: tid(),
-	itable % :: tid()
+	maxsize :: non_neg_integer(),
+	table :: tab(),
+	itable :: tab()
 }).
 
 %% Table with the key, value and timestamp
@@ -163,7 +161,7 @@ timestamp() ->
 -include_lib("eunit/include/eunit.hrl").
 -define(BATCHSIZE, 10000).
 
-etscache_test_() ->
+ets_cache_test_() ->
     {setup,
         fun start/0,
         fun stop/1,
@@ -181,7 +179,7 @@ etscache_test_() ->
     }.
 
 start() ->
-    C = etscache:new(100000),
+    C = ets_cache:new(100000),
     % fill the cache
     [put_new(C, "kk" ++ integer_to_list(I), <<"">>) || I <- lists:seq(1, 100000)],
     C.
